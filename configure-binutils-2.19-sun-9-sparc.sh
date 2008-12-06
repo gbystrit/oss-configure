acc14ededdfc0ae27c9300655423790700f9fb2f
gbystrit <gbystrit@faf13d3b-af0d-0410-97ba-fcc57a00092e>
2008-12-06 02:22:11 +0000

    Added configure scripts for binutils-2.19 and texinfo-4.11.
    git-svn-id: https://svn.code.sf.net/p/stemkit/code@2723 faf13d3b-af0d-0410-97ba-fcc57a00092e

---
+++ configure-binutils-2.19-sun-9-sparc.sh
@@ -0,0 +1,1 @@
+CC=gcc-3.4.3 CXX=g++-3.4.3 /opt/quest/source/binutils-2.19/configure --prefix=/opt/quest/platform/sun-9-sparc/binutils-2.19
---
+++ configure-texinfo-4.11-rh-30-x86.sh
@@ -0,0 +1,1 @@
+CC=gcc-3.4.3 CXX=g++-3.4.3 /opt/quest/source/texinfo-4.11/configure --prefix=/opt/quest/platform/rh-30-x86/texinfo-4.11

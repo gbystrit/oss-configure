97cce573360e8d5185723c675c90a3bad528181a
gbystrit <gbystrit@faf13d3b-af0d-0410-97ba-fcc57a00092e>
2008-12-06 10:56:32 +0000

    Building binutils with gcc-4.3 now. Added a configure invocation script for gcc 4.3.2.
    git-svn-id: https://svn.code.sf.net/p/stemkit/code@2731 faf13d3b-af0d-0410-97ba-fcc57a00092e

--- configure/binutils-2.19-rh-30-x86.sh
+++ configure/binutils-2.19-rh-30-x86.sh
@@ -1,1 +1,1 @@
-CC=gcc-3.4.3 CXX=g++-3.4.3 /opt/quest/source/binutils-2.19/configure --prefix=/opt/quest/platform/rh-30-x86/binutils-2.19
+CC=gcc-4.3 CXX=g++-4.3 /opt/quest/source/binutils-2.19/configure --prefix=/opt/quest/platform/rh-30-x86/binutils-2.19

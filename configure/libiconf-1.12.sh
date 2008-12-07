11ce7ae9e9704ff9f78ed1f68df06e6caf9dd0ab
gbystrit <gbystrit@faf13d3b-af0d-0410-97ba-fcc57a00092e>
2008-12-07 19:33:52 +0000

    Added lzma and libiconv.
    git-svn-id: https://svn.code.sf.net/p/stemkit/code@2737 faf13d3b-af0d-0410-97ba-fcc57a00092e

---
+++ configure/libiconf-1.12.sh
@@ -0,0 +1,1 @@
+CC=gcc-3.4 CXX=g++-3.4 /opt/quest/source/libiconv-1.12/configure --prefix=/opt/quest/platform/${SYSID:?}/libiconv-1.12 --disable-shared
---
+++ configure/lzma-4.32.7.sh
@@ -0,0 +1,3 @@
+LD_LIBRARY_PATH=/opt/quest/platform/${SYSID:?}/lib \
+CC=gcc-4.3 CXX=g++-4.3 \
+/opt/quest/source/lzma-4.32.7/configure --prefix=/opt/quest/platform/${SYSID:?}/lzma-4.32

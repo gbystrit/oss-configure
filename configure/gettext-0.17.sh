315517d7b12e58c93ce6a4930d15fca35dac50fc
gbystrit <gbystrit@faf13d3b-af0d-0410-97ba-fcc57a00092e>
2008-12-10 17:24:07 +0000

    More software. Platform notes for AIX.
    git-svn-id: https://svn.code.sf.net/p/stemkit/code@2748 faf13d3b-af0d-0410-97ba-fcc57a00092e

---
+++ configure/gettext-0.17.sh
@@ -0,0 +1,2 @@
+CONFIG_SHELL="${CONFIG_SHELL:=ksh}" CC=gcc-3.4.3 CXX=g++-3.4.3 \
+/opt/quest/source/gettext-0.17/configure --prefix=/opt/quest/platform/${SYSID:?}/gettext-0.17
--- configure/libiconf-1.12.sh
+++ configure/libiconf-1.12.sh
@@ -1,2 +1,2 @@
-CONFIG_SHELL="${CONFIG_SHELL:=ksh}" CC=gcc-3.4 CXX=g++-3.4 \
+CONFIG_SHELL="${CONFIG_SHELL:=ksh}" CC=gcc-3.4.3 CXX=g++-3.4.3 \
 /opt/quest/source/libiconv-1.12/configure --prefix=/opt/quest/platform/${SYSID:?}/libiconv-1.12 --disable-shared

13fa9be5f38b58f3bd996ee598a2de1e63dbde67
gbystrit <gbystrit@faf13d3b-af0d-0410-97ba-fcc57a00092e>
2009-01-04 07:45:19 +0000

    Making shared libraries for ncurses.
    git-svn-id: https://svn.code.sf.net/p/stemkit/code@2777 faf13d3b-af0d-0410-97ba-fcc57a00092e

--- configure/ncurses-5.7.sh
+++ configure/ncurses-5.7.sh
@@ -3,6 +3,8 @@
 . /opt/quest/source/configure/setup-build-environment.sh
 
 trace_and_run ${SOURCE}/ncurses-5.7/configure --prefix=${ROOT}/ncurses-5.7 \
+  --with-shared \
   --without-cxx \
   --without-cxx-binding \
+  "${@}"
 

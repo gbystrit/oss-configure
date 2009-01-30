ed8c093674fd75392861e73528073a69a4aafe99
gbystrit <gbystrit@faf13d3b-af0d-0410-97ba-fcc57a00092e>
2009-01-30 00:13:36 +0000

    Added rlwrap-0.30.
    git-svn-id: https://svn.code.sf.net/p/stemkit/code@2803 faf13d3b-af0d-0410-97ba-fcc57a00092e

---
+++ configure/rlwrap-0.30.sh
@@ -0,0 +1,12 @@
+# vim: set sw=2 tw=0:
+
+. `dirname "${0}"`/setup/build-environment.sh
+
+setup_dependencies \
+  libiconv-1.12 \
+  zlib-1.2.3 \
+  readline-5.2 \
+
+trace_and_run ${SOURCE}/rlwrap-0.30/configure `root_prefixes "rlwrap-0.30"` \
+  "${@}"
+

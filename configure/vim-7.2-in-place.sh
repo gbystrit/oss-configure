14871ef643d75796d6adb1bf8e838d492c950a22
gbystrit <gbystrit@faf13d3b-af0d-0410-97ba-fcc57a00092e>
2009-02-13 19:57:04 +0000

    Fixing invocation scripts for vim and cppunit.
    git-svn-id: https://svn.code.sf.net/p/stemkit/code@2809 faf13d3b-af0d-0410-97ba-fcc57a00092e

--- configure/vim-7.2-in-place.sh
+++ configure/vim-7.2-in-place.sh
@@ -5,36 +5,35 @@
 setup_feature vim-7.2
 setup_dependencies ncurses-5.7
 
-trace_and_run ./configure --prefix=${ROOT}/vim-7.2 \
-  --prefix=/opt/quest/platform/${SYSID:?}/vim-7.2  \
-  --disable-xsmp                                   \
-  --disable-xsmp-interact                          \
-  --disable-mzschemeinterp                         \
-  --disable-perlinterp                             \
-  --disable-pythoninterp                           \
-  --disable-tclinterp                              \
-  --disable-rubyinterp                             \
-  --enable-cscope                                  \
-  --disable-workshop                               \
-  --disable-netbeans                               \
-  --disable-sniff                                  \
-  --disable-multibyte                              \
-  --disable-hangulinput                            \
-  --disable-xim                                    \
-  --disable-fontset                                \
-  --disable-gui                                    \
-  --disable-gtk-check                              \
-  --disable-gtk2-check                             \
-  --disable-gnome-check                            \
-  --disable-motif-check                            \
-  --disable-athena-check                           \
-  --disable-nextaw-check                           \
-  --disable-carbon-check                           \
-  --disable-gtktest                                \
-  --disable-acl                                    \
-  --disable-gpm                                    \
-  --disable-sysmouse                               \
-  --disable-nls                                    \
-  --without-x                                      \
-  --without-gnome                                  \
+trace_and_run ./configure `root_prefixes "vim-7.2"` \
+  --disable-xsmp                                    \
+  --disable-xsmp-interact                           \
+  --disable-mzschemeinterp                          \
+  --disable-perlinterp                              \
+  --disable-pythoninterp                            \
+  --disable-tclinterp                               \
+  --disable-rubyinterp                              \
+  --enable-cscope                                   \
+  --disable-workshop                                \
+  --disable-netbeans                                \
+  --disable-sniff                                   \
+  --disable-multibyte                               \
+  --disable-hangulinput                             \
+  --disable-xim                                     \
+  --disable-fontset                                 \
+  --disable-gui                                     \
+  --disable-gtk-check                               \
+  --disable-gtk2-check                              \
+  --disable-gnome-check                             \
+  --disable-motif-check                             \
+  --disable-athena-check                            \
+  --disable-nextaw-check                            \
+  --disable-carbon-check                            \
+  --disable-gtktest                                 \
+  --disable-acl                                     \
+  --disable-gpm                                     \
+  --disable-sysmouse                                \
+  --disable-nls                                     \
+  --without-x                                       \
+  --without-gnome                                   \
   "${@}"

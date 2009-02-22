4761e2299a88d37d79033fe993244604f8e97603
gbystrit <gbystrit@faf13d3b-af0d-0410-97ba-fcc57a00092e>
2009-02-22 19:17:32 +0000

    Got rid of swig-1.3.36 in favor of swig-1.3.31 and that latter proved to be more stable.
    git-svn-id: https://svn.code.sf.net/p/stemkit/code@2812 faf13d3b-af0d-0410-97ba-fcc57a00092e

--- configure/swig-1.3.31.sh
+++ configure/swig-1.3.31.sh
@@ -2,5 +2,5 @@
 
 . `dirname "${0}"`/setup/build-environment.sh
 
-trace_and_run ${SOURCE}/swig-1.3.36/configure --prefix=${ROOT}/swig-1.3 \
+trace_and_run ${SOURCE}/swig-1.3.31/configure --prefix=${ROOT}/swig-1.3 \
   "${@}"

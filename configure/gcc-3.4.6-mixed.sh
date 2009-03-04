45700435709e3388ce9dc2230e06ac3ddc6241f0
gbystrit <gbystrit@faf13d3b-af0d-0410-97ba-fcc57a00092e>
2009-03-04 00:50:20 +0000

    Made it possible to build 64-bit gcc in a separate directory from 32 bit.
    git-svn-id: https://svn.code.sf.net/p/stemkit/code@2813 faf13d3b-af0d-0410-97ba-fcc57a00092e

--- configure/gcc-3.4.6-mixed.sh
+++ configure/gcc-3.4.6-mixed.sh
@@ -1,14 +1,14 @@
 # vim: set tw=0:
 
-. `dirname "${0}"`/setup/build-environment.sh
+. `dirname "${0}"`/setup/gcc-build-environment.sh
 
 setup_dependencies \
   libiconv-1.12
 
-trace_and_run ${SOURCE}/gcc-3.4.6/configure --prefix=${ROOT}/gcc-3.4 \
+trace_and_run ${SOURCE}/gcc-3.4.6/configure `root_prefixes "gcc${GCC_ABI}-3.4"` \
   --enable-languages=c,c++ \
   --without-gnu-ld --with-ld="/usr/ccs/bin/ld" \
-  --with-gnu-as --with-as="${ROOT}/binutils-2.19/bin/as" \
+  --with-gnu-as --with-as="${ROOT}/binutils${GCC_ABI}-2.19/bin/as" \
   --enable-shared \
   --enable-threads=posix \
   "${@}"

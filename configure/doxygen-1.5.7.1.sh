9253df2d27447a52a61422e1595154ecc94d6a12
gbystrit <gbystrit@faf13d3b-af0d-0410-97ba-fcc57a00092e>
2009-03-04 01:25:43 +0000

    Building doxygen now.
    git-svn-id: https://svn.code.sf.net/p/stemkit/code@2817 faf13d3b-af0d-0410-97ba-fcc57a00092e

--- configure/doxygen-1.5.7.1.sh
+++
@@ -1,5 +0,0 @@
-# vim: set tw=0:
-CONFIG_SHELL="${CONFIG_SHELL:=ksh}" \
-CC="${CC:=gcc-3.4}" \
-CXX="${CXX:=g++-3.4}" \
-/opt/quest/source/doxygen-1.5.7.1/configure --prefix=/opt/quest/platform/${SYSID:?}/doxygen-1.5
---
+++ configure/doxygen-1.5.8-in-place.sh
@@ -0,0 +1,14 @@
+# vim: set sw=2:
+
+. `dirname "${0}"`/setup/in-place.sh
+. `dirname "${0}"`/setup/gcc-build-environment.sh
+
+setup_feature doxygen-1.5.8
+PATH=${GCC_ROOT}/bin:${PATH} export PATH
+gcc -v
+
+trace_and_run ./configure --prefix ${ROOT}/doxygen-1.5 \
+  "${@}"
+
+sleep 5
+trace_and_run make

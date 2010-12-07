99d7392d88b2941ccb4a5ca2affdda2a9520c300
gbystrit <gbystrit@faf13d3b-af0d-0410-97ba-fcc57a00092e>
2010-12-07 02:17:20 +0000

    Added dependencies for sqlite3.
    git-svn-id: https://svn.code.sf.net/p/stemkit/code@2846 faf13d3b-af0d-0410-97ba-fcc57a00092e

--- configure/sqlite-3.7.3.sh
+++ configure/sqlite-3.7.3.sh
@@ -3,6 +3,10 @@
 . `dirname "${0}"`/setup/build-environment.sh
 
 setup_dependencies \
+  zlib-1.2.3 \
+  ncurses-5.7 \
+  readline-5.2 \
+  libiconv-1.12 \
 
 trace_and_run ${SOURCE}/sqlite-3.7.3/configure `root_prefixes "sqlite-3.7"` \
   "${@}"
